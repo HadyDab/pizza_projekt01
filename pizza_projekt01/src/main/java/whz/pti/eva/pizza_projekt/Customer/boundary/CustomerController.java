428a723e03679795c5a44522f2f6bab7c5d2b903
Hah16gw8 <Hamzat.Danladi.Hassan.gw8@fh-zwickau.de>
2017-12-06 12:50:04 +0100

    controller class erzeugt html datei user details erstellt methode show user details in controller implementiert

--- pizza_projekt01/src/main/java/whz/pti/eva/pizza_projekt/Customer/boundary/CustomerController.java
+++ pizza_projekt01/src/main/java/whz/pti/eva/pizza_projekt/Customer/boundary/CustomerController.java
@@ -1,9 +1,35 @@
 package whz.pti.eva.pizza_projekt.Customer.boundary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import whz.pti.eva.pizza_projekt.Customer.domain.Address;
+import whz.pti.eva.pizza_projekt.Customer.domain.Customer;
+import whz.pti.eva.pizza_projekt.Customer.service.CustomerService;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
 
-    
+
+    CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+
+    @RequestMapping("user_details")
+    public String showUserDetails(Model model){
+    Customer customer = customerService.getCustomerByLoginName("letss");
+    List<Address> customerAddress = customerService.getAdressesForCustomer(customer.getLoginName());
+        model.addAttribute(customer);
+        model.addAttribute("customerAddress",customerAddress);
+        System.out.println(customerAddress.get(0).getStreet());
+        return "user_details";
+    }
+
 }
